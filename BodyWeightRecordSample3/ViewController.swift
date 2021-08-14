9a2559684b46b5404962db99755ec8e706903268
kapisan <60970645+kapisan@users.noreply.github.com>
2021-08-14 17:37:26 +0900

    Initial Commit

---
+++ BodyWeightRecordSample3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BodyWeightRecordSample3
+//
+//  Created by niwa  shuhei on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
